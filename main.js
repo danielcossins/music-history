e111bef9d13169e267b5a6fc4a600399496b2220
danielcossins11 <daniel.cossins@yahoo.com>
2015-07-15 10:22:40 -0500

    title, artist, and album add automatically

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album App
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
 //var newSongs = [];
-var songTitles = [""];
-var songArtist = [""];
-var songAlbum = [""];
-var songGenre = [""];
+var songTitles = [];
+var songArtist = [];
+var songAlbum = [];
+var songGenre = [];
 for(var i=0; i<songs.length; i++){
   /*
   songs[i]=songs[i].replace("!", "");
@@ -22,30 +22,50 @@ for(var i=0; i<songs.length; i++){
   songs[i]=songs[i].replace(/[!*@(]/, "");
   songs[i]=songs[i].replace(">", "-");
 
+
+  //-----title
   var place = songs[i].indexOf("-");
   for(var j=0; j<place; j++){
     songTitles.push("");
   	songTitles[i]+=songs[i].charAt(j);
   }
-  // // //--retrieves title
-  // for(var j=0; j<newSongs[i].length; j++){
-  //   var letter=newSongs[i].charAt(j);
-  //   if(letter==="-"){
-  //     break;
-  //   }
-  //   else{
-  //     songTitles[i] += letter;
-  //   }
-  // }
+  //-----artist
+  var artistPlace = songs[i].indexOf("on ");
+  for(var k=place; k<artistPlace; k++){
+    songArtist.push("");
+    songArtist[i]+=songs[i].charAt(k);
+  }
+    //-----album
+  var albumPlace = songs[i].length;//indexOf("album");
+  for(var l=artistPlace; l<albumPlace; l++){
+    songAlbum.push("");
+    songAlbum[i]+=songs[i].charAt(l);
+  }
+    //-----genre
+  
 }
-alert(songs);
 
 var songTitleElements = [];
+var artistElements = [];
+var albumElements = [];
+var genreElements = [];
 for(var i=0; i<songs.length; i++){
-  var id="song"+(i+1);
-  alert(id);
-  songTitleElements[i] = document.getElementById(id);
+  //-----title
+  var idTitle="song"+(i+1);
+  songTitleElements[i] = document.getElementById(idTitle);
   songTitleElements[i].innerHTML = songTitles[i];
+  //-----artist
+  var idArtist="artist"+(i+1);
+  artistElements[i] = document.getElementById(idArtist);
+  artistElements[i].innerHTML = songArtist[i];
+  //-----album
+  var idAlbum="album"+(i+1);
+  albumElements[i] = document.getElementById(idAlbum);
+  albumElements[i].innerHTML = songAlbum[i];
+  //-----genre
+  // var idGenre="album"+(i+1);
+  // genreElements[i] = document.getElementById(idGenre);
+  // genreElements[i].innerHTML = songGenre[i];
 }
 
 //var songTitleElements = document.getElementById("song1");
